7956a2861a0b8f4c518a50875a70a9a1d1abf74f
DanelMadrazo <dmadra25@student.aau.dk>
2026-02-18 15:45:46 +0100

    Visualize the result and clean code

--- mandelbrot.py
+++ mandelbrot.py
@@ -5,29 +5,46 @@ Course : Numerical Scientific Computing 2026
 """
 import numpy as np
 import time
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
-start = time.time()
-xmin, xmax, ymin, ymax = -2, 1, -1.5, 1.5
-res = 1024
-
-x = np.linspace(xmin, xmax, res)
-y = np.linspace(ymin, ymax, res)
-
-def mandelbrot_point(x, y, max_iter = 100):
-    c = x + 1j * y
+#STEP 2
+def mandelbrot_point(c, max_iter = 100):
     z = 0j
     for n in range(max_iter):
         z = z**2 + c 
         if abs(z) > 2:
-            break    
-    return n
+            return n    
+    return max_iter
 
-iteration_num = np.zeros((res, res))
+#STEP 3
+def compute_mandelbrot(xmin, xmax, ymin, ymax, x_res, y_res, max_iter = 100):
+    x = np.linspace(xmin, xmax, x_res)
+    y = np.linspace(ymin, ymax, y_res)
+    
+    iteration_num = np.zeros((y_res, x_res))
+    
+    for i in range(y_res):
+        for j in range(x_res):
+            c = complex(x[j], y[i])
+            n = mandelbrot_point(c, max_iter)
+            iteration_num[i, j] = n
+    return iteration_num
 
-for i in range(res):
-    for j in range (res):
-        iteration_num[i, j] = mandelbrot_point(x[j], y[j])
-          
+xmin, xmax, ymin, ymax = -2, 1, -1.5, 1.5
+res_x, res_y = 1024, 1024
+
+#STEP 4
+start = time.time()
+result = compute_mandelbrot(xmin, xmax, ymin, ymax, res_x, res_y)
 elapsed = time.time() - start
-print(elapsed)
+
+print(f"Computation took {elapsed:.3f} seconds")
+
+#STEP 5
+plt.figure()
+plt.imshow(result, cmap = 'viridis')
+plt.title("Mandelbrot")
+plt.xlabel("Real (Re)")
+plt.ylabel("Imaginary (Im)")
+plt.show()
+        
