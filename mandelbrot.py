6e4e69d24b0f62e235ae3e6d94b7118377cf9a1b
DanelMadrazo <dmadra25@student.aau.dk>
2026-02-18 14:27:29 +0100

    Implement mandelbrot point function

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,19 +1,22 @@
 """
 Mandelbrot Set Generator
-Author : [ Your Name ]
+Author : [ Danel Madrazo ]
 Course : Numerical Scientific Computing 2026
 """
-def f(x):
-    """
-    Example function .
-    Parameters
-    ----------
-    x : float
-    Input value
-    Returns
-    -------
-    float
-    Output value
-    """
-    # TODO : Implement the algorithm
-pass
+import numpy as np
+x = -0.1
+y = 0.1
+
+c = x + 1j * y
+
+z = np.zeros(100, dtype=complex)
+for n in range(99):
+    z[n+1] = z[n]**2 + c
+    
+    if abs(z[n+1]) > 2:
+        print(f"point scapes! iteration number:{n+1}")
+        break 
+else: 
+    print(f"Point didnt scape, iteration number: {n+1}")
+
+            
