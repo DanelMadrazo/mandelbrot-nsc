9a56f636811c90288aba143e3b1b712b1952f6a0
DanelMadrazo <dmadra25@student.aau.dk>
2026-02-19 14:13:24 +0100

    L2, Milestone 1 Basic Arrays
    Create the complex grid C for the Mandelbrot set

--- mandelbrot.py
+++ mandelbrot.py
@@ -33,15 +33,22 @@ def compute_mandelbrot(xmin, xmax, ymin, ymax, x_res, y_res, max_iter = 100):
 xmin, xmax, ymin, ymax = -2, 1, -1.5, 1.5
 res_x, res_y = 1024, 1024
 
-#STEP 4
-start = time.time()
-result = compute_mandelbrot(xmin, xmax, ymin, ymax, res_x, res_y)
-elapsed = time.time() - start
+x = np . linspace ( xmin , xmax, res_x) # 1024 x- values
+y = np . linspace ( ymin , ymax , res_y) # 1024 y- values
+X , Y = np . meshgrid (x , y) # 2D grids
+C = X + 1j* Y # Complex grid
+print (f" Shape : {C. shape }") # (1024 , 1024)
+print (f" Type : {C. dtype }") # complex128
+
+# #STEP 4
+# start = time.time()
+# result = compute_mandelbrot(xmin, xmax, ymin, ymax, res_x, res_y)
+# elapsed = time.time() - start
 
 #1024x1024 resolution takes around 4 seconds
 #2048x2048 resolution takes around 17 seconds
 
-print(f"Computation took {elapsed:.3f} seconds")
+# print(f"Computation took {elapsed:.3f} seconds")
 
 #Lecture 2, new time measurement function
 def benchmark ( func , * args , n_runs =3) :
@@ -55,7 +62,7 @@ def benchmark ( func , * args , n_runs =3) :
     print (f" Median : { median_t:.4f}s "
     f"( min ={ min( times ):.4f}, max ={ max( times ):.4f})")
     return median_t , result
-t , M = benchmark (compute_mandelbrot, -2, 1, -1.5 , 1.5 , 1024 , 1024 , 100)
+t , M = benchmark (compute_mandelbrot, xmin, xmax, ymin , ymax , res_x , res_y , 100)
 
 #STEP 5
 plt.figure()
