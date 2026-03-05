d28449031d014abf90ba3855908873852b515c79
DanelMadrazo <dmadra25@student.aau.dk>
2026-03-05 15:41:13 +0100

    Monte Carlo Exercise 3
    workers | time (s) | speedup Sp | efficiency Ep (%)
    -------------------------------------------------------
          1 |    2.427 |       0.72x |             72%
          2 |    1.343 |       1.29x |             65%
          3 |    1.450 |       1.20x |             40%
          4 |    1.299 |       1.34x |             33%
          5 |    1.420 |       1.22x |             24%
          6 |    1.284 |       1.35x |             23%
          7 |    1.227 |       1.41x |             20%
          8 |    1.653 |       1.05x |             13%
    Maximum speedup (Sp*) = 1.41x at p* = 7 workers
    Implied serial fraction (s) = 65.81%

--- MonteCarlo_Exercises.py
+++ MonteCarlo_Exercises.py
@@ -31,6 +31,8 @@ def estimate_pi_parallel(num_samples, num_processes=4):
     return 4 * sum(results) / num_samples
 
 
+
+
 if __name__ == '__main__':
     num_samples = 10000000
     times = []
@@ -39,8 +41,15 @@ if __name__ == '__main__':
         pi_estimate = estimate_pi_serial(num_samples)
         times.append(time.perf_counter() - t0)
     t_serial = statistics.median(times)
+    print(f"=== Baseline Serial ===")
     print(f"pi estimate: {pi_estimate:.6f} (error: {abs(pi_estimate-math.pi):.6f})")
     print(f"Serial time: {t_serial:.3f}s")
+    
+    max_speedup = 0
+    best_p = 1
+    
+    print("workers | time (s) | speedup Sp | efficiency Ep (%)")
+    print("-" * 55)
 
     for num_proc in range(1, os.cpu_count() + 1):
         times = []
@@ -48,5 +57,27 @@ if __name__ == '__main__':
             t0 = time.perf_counter()
             pi_est = estimate_pi_parallel(num_samples, num_proc)
             times.append(time.perf_counter() - t0)
-        t = statistics.median(times)
-        print(f"{num_proc:2d} workers:{t:.3f}s pi={pi_est:.6f}")   
+        t_par = statistics.median(times)
+        #print(f"{num_proc:2d} workers:{t_par:.3f}s pi={pi_est:.6f}") 
+        
+        #E3
+        speedup = t_serial / t_par
+        efficiency = (speedup / num_proc) * 100
+        
+        print(f"{num_proc:7d} | {t_par:8.3f} | {speedup:10.2f}x | {efficiency:14.0f}%")
+        
+        if speedup > max_speedup:
+            max_speedup = speedup
+            best_p = num_proc
+            
+    print(f"Maximum speedup (Sp*) = {max_speedup:.2f}x at p* = {best_p} workers")
+    
+    if best_p > 1:
+        s = (1 / max_speedup - 1 / best_p) / (1 - 1 / best_p)
+        print(f"Implied serial fraction (s) = {s * 100:.2f}%")
+    else:
+        print("1 Worker was the best, cannot calculate 's'.")
+        
+    
+    
+
