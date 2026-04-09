fbd996aaca910cfe912fab82ccb46414d0d9ce42
DanelMadrazo <dmadra25@student.aau.dk>
2026-04-09 14:39:05 +0200

    E2: Catastrophic Cancellation

--- Machine_Epsilon.py
+++ Machine_Epsilon.py
@@ -10,11 +10,23 @@ def find_machine_epsilon(dtype=np.float64):
     while dtype(1.0) + eps / dtype(2.0) != dtype(1.0):
         eps = eps / dtype(2.0)
     return eps
+def quadratic_naive(a, b, c):
+    t = type(a)
+    disc = t(np.sqrt(b*b - t(4)*a*c))
+    x1 = (-b + disc) / (t(2)*a)
+    x2 = (-b - disc) / (t(2)*a)
+    return x1, x2
 
-for dtype in [np.float16, np.float32, np.float64]:
-    computed = find_machine_epsilon(dtype)
-    reference = np.finfo(dtype).eps
-    print(f'{dtype.__name__}:')
-    print(f" Computed: {float(computed):.4e}")
-    print(f" np.finfo: {float(reference):.4e}")
-    print()
+# for dtype in [np.float16, np.float32, np.float64]:
+#     computed = find_machine_epsilon(dtype)
+#     reference = np.finfo(dtype).eps
+#     print(f'{dtype.__name__}:')
+#     print(f" Computed: {float(computed):.4e}")
+#     print(f" np.finfo: {float(reference):.4e}")
+#     print()
+
+for dtype in [np.float32, np.float64]:
+    a, b, c = dtype(1.0), dtype(-10000.0001), dtype(1.0)
+    x1, x2 = quadratic_naive(a, b, c)
+    print(f'{dtype.__name__}: x1 = {float(x1):.4f}, x2 = {float(x2):.10f}')
+    
